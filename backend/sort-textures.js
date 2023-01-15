bb647ba6e89325683760c809ddadd6a76a68a00e
tecate <james@hellco.net>
2023-01-15 13:53:27 -0500

    backend outputs better formated json now

--- backend/sort-textures.js
+++ backend/sort-textures.js
@@ -2,9 +2,14 @@ var fs = require('fs');
 
 const skyboxDir = '../skyboxes/';
 const outputFile = 'skyboxes.json';
-const needsRenaming = new RegExp('^.*(up|dn|lf|rt|ft|bk)\.jpg{1}$', 'i');
+const needsRenaming = new RegExp('^.*(up|dn|lf|rt|ft|bk)\.jpg$', 'i');
+const isSkybox = new RegExp('^.*(-pos-x|-neg-x|-pos-y|-neg-y|-pos-z|-neg-z)\.jpg$', 'i');
+const isText = new RegExp('^.*\.txt$', 'i');
+
 var fileType = ".jpg";
 var itemsProcessed = 0;
+var skyboxes = {};
+
 
 fs.readdir(skyboxDir, (err, files) => {
     files.forEach(file => {
@@ -38,13 +43,52 @@ fs.readdir(skyboxDir, (err, files) => {
 });
 
 function renameFinished() {
+    itemsProcessed = 0;
     fs.readdir(skyboxDir, (err, files) => {
-        console.log("writing skyboxes to", outputFile);
-        console.log(files);
-        try {
-            fs.writeFileSync(outputFile, JSON.stringify(files))
-        } catch (err) {
-            console.error(err)
-        }
+        // group into sets
+        files.forEach(file => {
+            if (isSkybox.test(file) && !(file.slice(0, -10) in skyboxes)) {
+                // skyboxes[file.slice(0, -10)][file.slice(0, -4).slice(-5)] = file;
+                skyboxes[file.slice(0, -10)] = {
+                    "pos-x": file.slice(0, -10) + "-pos-x.jpg",
+                    "neg-x": file.slice(0, -10) + "-neg-x.jpg",
+                    "pos-y": file.slice(0, -10) + "-pos-y.jpg",
+                    "neg-y": file.slice(0, -10) + "-neg-y.jpg",
+                    "pos-z": file.slice(0, -10) + "-pos-z.jpg",
+                    "neg-z": file.slice(0, -10) + "-neg-z.jpg"
+                };
+                itemsProcessed++
+                if (itemsProcessed === files.length) {
+                    jsonFinished();
+                }
+            } else if (isText.test(file)) {
+                fs.readFile(skyboxDir + file, 'utf8', (err, data) => {
+                    if (err) {
+                      console.error(err);
+                      return;
+                    }
+                    skyboxes[file.slice(0, -4)].source = data;
+                    itemsProcessed++
+                    if (itemsProcessed === files.length) {
+                        jsonFinished();
+                    }
+                });
+            } else {
+                itemsProcessed++
+                if (itemsProcessed === files.length) {
+                    jsonFinished();
+                }
+            }
+        });
     });
+}
+
+function jsonFinished() {
+    console.log(skyboxes);
+    console.log("writing skyboxes to", outputFile);
+    try {
+        fs.writeFileSync(outputFile, JSON.stringify(skyboxes))
+    } catch (err) {
+        console.error(err)
+    }
 }
